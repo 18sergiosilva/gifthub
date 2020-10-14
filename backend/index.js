1f48349f1df040b71ff279ebdb0584d3264e104c
Leo <leoaguilarsanchez97@gmail.com>
2020-10-14 17:50:17 -0600

    Agregada version de prueba para jenkins

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 
 //Entry
 app.get('/', function (req, res) {
-    res.send('API FUNCIONANDO CORRECTAMENTE');
+    res.send('API FUNCIONANDO CORRECTAMENTE V2');
 });
 
 const port = process.env.PORT || 5000;
--- backend/tests/main.test.js
+++ backend/tests/main.test.js
@@ -3,8 +3,8 @@ const api = require('../index');
 const app = api.app;
 
 describe('GET /', function() {
-    it('/ responde con API FUNCIONANDO CORRECTAMENTE', done => {
-        request(app).get('/').expect('API FUNCIONANDO CORRECTAMENTE');
+    it('/ responde con API FUNCIONANDO CORRECTAMENTE V2', done => {
+        request(app).get('/').expect('API FUNCIONANDO CORRECTAMENTE V2');
         done();
     });
 });
