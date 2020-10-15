23772bcdf5ab4ece1dcb7e7be85c6f4a3aceee32
Leo <leoaguilarsanchez97@gmail.com>
2020-10-14 22:13:46 -0600

    Finalizado feature de conexion a la bd

--- backend/tests/main.test.js
+++ backend/tests/main.test.js
@@ -1,10 +1,21 @@
 const request = require('supertest');
 const api = require('../index');
+
 const app = api.app;
+
+const { expect } = require('chai');
 
 describe('GET /', function() {
     it('/ responde con API FUNCIONANDO CORRECTAMENTE V2', done => {
-        request(app).get('/').expect('API FUNCIONANDO CORRECTAMENTE V2');
-        done();
+        request(app).get('/').end(function(error, result) {
+            if (error) {
+                console.log("ERROR: ");
+                console.log(error);
+            }
+
+            expect(result.text).to.equal("API y MongoDB funcionando correctamente");
+
+            done();
+        });
     });
 });
