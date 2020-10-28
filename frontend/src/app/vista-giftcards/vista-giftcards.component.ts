5fc2bf300e899441e772596ab958a70405206898
Sergio Silva <18sergiosilva@gmail.com>
2020-10-28 02:11:53 -0600

    Estructura grafica de las giftcards terminada

--- frontend/src/app/vista-giftcards/vista-giftcards.component.ts
+++ frontend/src/app/vista-giftcards/vista-giftcards.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { ServicioVistaGiftcardsService } from '../services/servicio-vista-giftcards.service'
+import { first } from 'rxjs/operators';
+//import { Venta, Producto, Factura } from '../models/modelos';
 
 @Component({
   selector: 'app-vista-giftcards',
@@ -7,9 +12,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VistaGiftcardsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private servicioVistaGiftcards: ServicioVistaGiftcardsService) { }
 
   ngOnInit(): void {
+    this.actualizarGiftcards();
+    this.getGiftcardsLocales();
+  }
+
+  actualizarGiftcards() {
+    this.servicioVistaGiftcards.actualizarGiftcards()
+      .pipe(first())
+      .subscribe(
+        data => {
+          console.log("data actualizada");
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  getGiftcardsLocales(){
+
+    this.servicioVistaGiftcards.getGiftcardsLocales()
+      .pipe(first())
+      .subscribe(
+        data => {
+          console.log("LA INFO ",data);
+        },
+        error => {
+          console.log(error);
+        });
+
   }
 
 }
