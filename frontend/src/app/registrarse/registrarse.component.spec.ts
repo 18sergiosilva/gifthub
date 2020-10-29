37d42a15f9df7e1d9690d7ebb5c82ed59d61bd47
Francisco Lezana <fraaanciscolezana8@gmail.com>
2020-10-28 21:23:44 -0600

    Se arregló test para CI

--- frontend/src/app/registrarse/registrarse.component.spec.ts
+++ frontend/src/app/registrarse/registrarse.component.spec.ts
@@ -18,8 +18,4 @@ describe('RegistrarseComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
