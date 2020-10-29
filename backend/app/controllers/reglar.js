43c5368e68c25424953d66530aac111f112ba792
Leo <leoaguilarsanchez97@gmail.com>
2020-10-29 03:29:07 -0600

    Fix 2 para que funcione con this

--- backend/app/controllers/reglar.js
+++ backend/app/controllers/reglar.js
@@ -109,35 +109,35 @@ exports.regalar = async function (req, res) {
         });
     }
 
-    let usuario1 = await this.obtenerUsuario(req.body.usuario1)
+    let usuario1 = await exports.obtenerUsuario(req.body.usuario1)
     if (!usuario1) {
         return res.status(400).send({
             message: `No se encontro el usuario ${req.body.usuario1}`
         });
     }
 
-    let usuario2 = await this.obtenerUsuario(req.body.usuario2)
+    let usuario2 = await exports.obtenerUsuario(req.body.usuario2)
     if (!usuario2) {
         return res.status(400).send({
             message: `No se encontro el usuario ${req.body.usuario2}`
         });
     }
 
-    usuario1 = this.modificarInventarioTrjetasUsuario1(usuario1, req.body.giftcards)
+    usuario1 = exports.modificarInventarioTrjetasUsuario1(usuario1, req.body.giftcards)
     if (usuario1.message) {
         return res.status(400).send({
             message: usuario1.message
         });
     }
 
-    usuario2 = this.modificarInventarioTrjetasUsuario2(usuario2, req.body.giftcards)
+    usuario2 = exports.modificarInventarioTrjetasUsuario2(usuario2, req.body.giftcards)
     if (usuario2.message) {
         return res.status(400).send({
             message: usuario2.message
         });
     }
 
-    usuario1 = await this.actualizarUsuario(usuario1);
+    usuario1 = await exports.actualizarUsuario(usuario1);
     if (usuario1.message != 'Usuario actualizado correctamente.'){
         console.log(`Error modificando las tarjetas de ${req.body.usuario1}: `, usuario1.message);
         return res.status(500).send({
@@ -145,7 +145,7 @@ exports.regalar = async function (req, res) {
         });
     }
 
-    usuario2 = await this.actualizarUsuario(usuario2);
+    usuario2 = await exports.actualizarUsuario(usuario2);
     if (usuario2.message != 'Usuario actualizado correctamente.'){
         console.log(`Error modificando las tarjetas de ${req.body.usuario2}: `, usuario2.message);
         return res.status(500).send({
