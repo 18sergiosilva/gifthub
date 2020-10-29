32284a50276b77ca0c8b876d337ca217bea236c8
David Salguero <mada.salgui@gmail.com>
2020-10-29 01:18:38 -0600

    Modificado login para pruebas de desarrollo

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -17,12 +17,16 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
   entrar() {
-    this.http.post('http://35.239.230.8:5000/usuario/' + this.correo,
-        {
-
-        }).toPromise().then((data: any) => {
-          console.log(data);
-        });
+    this.http.get('http://35.239.230.8:5000/usuario/' + this.correo)
+      .toPromise().then((data: any) => {
+        //if (data.usuario.contrasena === this.contra) {
+        if (true) {
+          localStorage.setItem('user', this.correo);
+          this.router.navigate(['home']);
+        } else {
+          this.cancelar();
+        }
+      });
   }
   cancelar() {
     this.correo = '';
