b98a36d964a4d2a5c78f0b4458134dacd1305e1f
Sergio Silva <18sergiosilva@gmail.com>
2020-10-31 01:33:27 -0600

    pruebas al componente de visualizar giftcards de 1 usuario terminado

--- frontend/src/app/inventario-giftcards/inventario-giftcards.component.spec.ts
+++ frontend/src/app/inventario-giftcards/inventario-giftcards.component.spec.ts
@@ -3,6 +3,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { Router, Routes } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { InventarioGiftcardsComponent } from './inventario-giftcards.component';
+import { Giftcard3 } from "../models/modelos";
 
 describe('InventarioGiftcardsComponent', () => {
   let component: InventarioGiftcardsComponent;
@@ -30,4 +31,52 @@ describe('InventarioGiftcardsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it("Debe retornar impreso al llamar al metodo imprimirError", async(() => {
+    expect(component.imprimirError("miError")).toContain("impreso");
+  }));
+
+  it("Debe retornar la disponibilidad de una tarjeta", async(() => {
+    const vistaGiftcard: Giftcard3 = {
+      nombre: "minombre",
+      imagen: "miimagen",
+      precio: 100,
+      id: 1,
+      displayName: "displayName",
+      availability: "1",
+      cantidad : 1,
+    };
+    component.giftcards.push(vistaGiftcard);
+    expect(component.disponibilidad(0)).toContain("1");
+  }));
+
+  it("Debe retornar la longitud del arreglo de giftcards", async(() => {
+    const vistaGiftcard: Giftcard3 = {
+      nombre: "minombre",
+      imagen: "miimagen",
+      precio: 100,
+      id: 1,
+      displayName: "displayName",
+      availability: "1",
+      cantidad : 1,
+    };
+    component.giftcards.push(vistaGiftcard);
+    expect(component.longitud()).toBeGreaterThan(0);
+  }));
+
+
+  it("Debe retornar verdadero al agregar 1 giftcard", async(() => {
+    const vistaGiftcard: Giftcard3 = {
+      nombre: "minombre",
+      imagen: "miimagen",
+      precio: 100,
+      id: 1,
+      displayName: "displayName",
+      availability: "1",
+      cantidad : 1,
+    };
+    component.giftcards.push(vistaGiftcard);
+    expect(component.agregarGiftcard(vistaGiftcard)).toBeTrue();
+  }));
+
 });
