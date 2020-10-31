e5d612c98a40a11e40aaaa5ddb53dbd62b9d51e0
Leo <leoaguilarsanchez97@gmail.com>
2020-10-31 04:11:29 -0600

    Eliminado tests para que sonarqube unicamente utilice los generados por el equipo de gifthub

--- backend/sonar-project.js
+++ backend/sonar-project.js
@@ -3,7 +3,7 @@ const sonarqubeScanner = require('./lib/sonarqube-scanner');
 sonarqubeScanner({
     serverUrl: 'http://localhost:9000',
     options: {
-        'sonar.sources': '.',
+        'sonar.sources': './app',
         'sonar.tests': 'tests',
         'sonar.inclusions': '**', // Entry point of your code
         'sonar.test.inclusions': 'tests/*.test.js',
