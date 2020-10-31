99bee770367fd35ed6087b0da45ac9dd85688849
Francisco Lezana <fraaanciscolezana8@gmail.com>
2020-10-31 14:45:26 -0600

    Se corrigió test

--- frontend/src/app/detalles-compra/detalles-compra.component.spec.ts
+++ frontend/src/app/detalles-compra/detalles-compra.component.spec.ts
@@ -18,8 +18,4 @@ describe('DetallesCompraComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
