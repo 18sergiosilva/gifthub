d9102c9476d5b5891bcd5916b71d133a0d66faa5
Edgar Aldana <arnoldo201@hotmail.com>
2020-11-01 19:56:23 -0600

    Se creo el metodo login

---
+++ backend/app/routes/login.js
@@ -0,0 +1,9 @@
+module.exports = app => {
+    const producto = require("../controllers/login");
+
+    var router = require("express").Router();
+
+    // Login
+    router.post("/", producto.findOne);
+
+};
