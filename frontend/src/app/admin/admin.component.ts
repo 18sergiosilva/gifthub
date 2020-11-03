5717ccc46132ef7804ae1a0458eb06dc97508308
Francisco Lezana <fraaanciscolezana8@gmail.com>
2020-11-02 22:01:15 -0600

    Se agregó vista de administrador

--- frontend/src/app/admin/admin.component.ts
+++ frontend/src/app/admin/admin.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { ServicioHistorialService } from '../services/servicio-historial.service';
+import { ActivatedRoute } from '@angular/router';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private http: HttpClient, private route: ActivatedRoute, private historialServ: ServicioHistorialService) { }
+
+  transacciones=[];
+  tarjetas=[];
 
   ngOnInit() {
+    this.getTransacciones();
+  }
+
+  getTransacciones(): boolean {
+    this.historialServ.getTransacciones()
+      .subscribe(
+        data => {
+          this.transacciones = data.transacciones;
+          this.getPrecios(this.transacciones);
+          return true;
+        },
+        error => {
+          //console.log(error);
+          return false;
+        });
+    return true;
+  }
+
+  getPrecios(arrTransacciones){
+    //console.log(arrTransacciones);
+    arrTransacciones.forEach(element => {
+      this.tarjetas.push(element.tarjetas);
+    });
   }
 
 }
