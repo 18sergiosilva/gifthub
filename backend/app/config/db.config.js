0baee1c4e32abe33a8e70c0910a77969fbb06326
Sergio Silva <3055753920301@ingenieria.usac.edu.gt>
2020-12-29 00:15:03 -0600

    cambios en base

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,5 +1,5 @@
 
 module.exports = {
-  url: "mongodb+srv://ayd:1234@gifthubdata.frnw3.gcp.mongodb.net/gifthub?retryWrites=true&w=majority",
+  url: "mongodb+srv://so2juegos:so2juegos@cluster.yeadg.mongodb.net/so2juegos?retryWrites=true&w=majority",
   testing: "mongodb+srv://ayd:1234@gifthubdata.frnw3.gcp.mongodb.net/testing?retryWrites=true&w=majority"
 };
